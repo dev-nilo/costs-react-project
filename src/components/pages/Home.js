a1fcd68f1bbf6145558d997464d3fcf39a9f8e34
Nilo Venturin Neto <nilo.neto@coroa.local>
2024-01-04 15:13:58 -0300

    adding styles to home

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,12 +1,13 @@
 import styles from './Home.module.css'
 import savings from '../../img/savings.svg'
+import LinkButton from '../layouts/LinkButton'
 
 function Home () {
     return (
         <section className={ styles.homeContainer }>
             <h1>Welcome to <span>Costs</span></h1>
             <p>Learn project management right now!</p>
-            <button><a href='/'>Start Project</a></button>
+            <LinkButton to="/newproject" text="Start Project" />
             <img src={ savings } alt='costs-pig' />
         </section>
     )
