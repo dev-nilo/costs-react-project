08cbd3c2185ad98144a86ab7afece6cca5672054
Nilo Venturin Neto <nilo.neto@coroa.local>
2024-01-04 10:29:32 -0300

    0401 navbar and footer

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/pages/Home";
 import Company from "./components/pages/Company";
 import Contact from "./components/pages/Contact";
 import NewProject from "./components/pages/NewProject";
 
 import Container from "./components/layouts/Container";
+import Navbar from "./components/layouts/Navbar";
+import Footer from "./components/layouts/Footer";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <div>
-          <Link to="/">Home</Link>
-          <Link to="/company">The Company</Link>
-          <Link to="/contact">Contact</Link>
-          <Link to="/newproject">New Project</Link>
-        </div>
+        <Navbar />
         <Container customClass="minHeight">
           <Routes>
             <Route exact path="/" element={<Home />} />
@@ -24,7 +21,7 @@ function App() {
             <Route path="/newproject" element={<NewProject />} />
           </Routes>
         </Container>
-        <p>Footer</p>
+        <Footer />
       </Router>
     
     </div>
